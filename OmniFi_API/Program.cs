34fb33c071b87643ab64a2a4cb57b34adafe7d86
yann <yann.matoka@gmail.com>
2024-06-20 17:23:08 +0200

    configure connectionstring for second device

--- OmniFi_API/Program.cs
+++ OmniFi_API/Program.cs
@@ -32,7 +32,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"))
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("SecondSQLConnection"))
     );
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
